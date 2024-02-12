82857f31aeb9e4f9eb825df09c90510a0f814254
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-02-11 21:32:55 -0700

    add h1 in stopwatch component and edit css styles

--- stopwatch-app/src/Stopwatch.jsx
+++ stopwatch-app/src/Stopwatch.jsx
@@ -39,16 +39,17 @@ export default function Stopwatch() {
     let seconds = Math.floor((elapsedTime / 1000) % 60);
     let milliseconds = Math.floor((elapsedTime % 1000) / 10);
 
-    hours = String(hours).padStart(2, "0")
-    minutes = String(minutes).padStart(2, "0")
-    seconds = String(seconds).padStart(2, "0")
-    milliseconds = String(milliseconds).padStart(2, "0")
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
 
     return `${hours}:${minutes}:${seconds}:${milliseconds}`;
   }
 
   return (
     <>
+      <h1>StopWatch</h1>
       <div className="stopwatch">
         <div className="display">{formatTime()}</div>
         <div className="controls">
