30c9662a81f7b71643b979676c8f095d58e3c613
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-03-23 21:04:58 -0600

    Initial commit

---
+++ password-generator/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-4xl text-center'>hi</h2>
+    </>
+  )
+}
+
+export default App
