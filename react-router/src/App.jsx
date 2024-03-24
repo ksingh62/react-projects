1f66730bdea096b88e533e0e3d8f3e2d30c7cce8
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-03-24 14:20:13 -0600

    Initial commit

---
+++ react-router/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h2 className='bg-red-500 text-center'>hi</h2>
+    </>
+  )
+}
+
+export default App
